ec04eb4100e158058a11840fc632bb48d8f13bb4
Jagadeswar DevOps <148185310+JagadeswarReddyPuthi@users.noreply.github.com>
2024-02-07 19:00:34 +0530

    Update install_student_app_tools.sh

--- install_student_app_tools.sh
+++ install_student_app_tools.sh
@@ -26,7 +26,7 @@ sudo su - devops -c "git config --global user.name 'devops'"
 sudo su - devops -c "git config --global user.email 'devops@gmail.com'"
 
 # Installing Java 11
-#sudo yum install java-11-openjdk-devel.x86_64 -y
+sudo yum install java-11-amazon-corretto.x86_64 -y
 
 sudo chown -R devops:devops /opt
 
@@ -41,44 +41,40 @@ sudo su - devops -c "ln -s /opt/maven/bin/mvn /usr/local/bin/mvn"
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.85/bin/apache-tomcat-9.0.85.tar.gz
 tar -xzvf apache-tomcat-9.0.85.tar.gz
 rm -rf apache-tomcat-9.0.85.tar.gz
-mv apache-tomcat-9.0.85 appserver
+mv apache-tomcat-9.0.85 tcserver
 
-echo '[Unit]
-        Description=Tomcat Server
-        After=syslog.target network.target
+echo  ' After=syslog.target network.target
 
         [Service]
         Type=forking
         User=devops
         Group=devops
 
-        Environment=JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.13.0.8-4.el8_5.x86_64
+        Environment=JAVA_HOME=/usr/lib/jvm/java-11-amazon-corretto.x86_64
         Environment='JAVA_OPTS=-Djava.awt.headless=true'
-        Environment=CATALINA_HOME=/opt/appserver/
-        Environment=CATALINA_BASE=/opt/appserver/
-        Environment=CATALINA_PID=/opt/appserver/temp/tomcat.pid
+        Environment=CATALINA_HOME=/opt/tcserver/
+        Environment=CATALINA_BASE=/opt/tcserver/
+        Environment=CATALINA_PID=/opt/tcserver/temp/tomcat.pid
         Environment='CATALINA_OPTS=-Xms512M -Xmx512M'
-        ExecStart=/opt/appserver/bin/catalina.sh start
-        ExecStop=/opt/appserver/bin/catalina.sh stop
+        ExecStart=/opt/tcserver/bin/catalina.sh start
+        ExecStop=/opt/tcserver/bin/catalina.sh stop
 
         [Install]
-        WantedBy=multi-user.target' > /etc/systemd/system/tomcat.service
+        WantedBy=multi-user.target ' > /etc/systemd/system/tomcat.service
 
 systemctl daemon-reload
 systemctl start tomcat
 systemctl enable tomcat
 
-# Setup Nginx
-
+# Install Nginx
 sudo yum install nginx.x86_64 -y
 systemctl enable nginx
 systemctl start nginx
 
-# Setup Maria DB
-
-sudo yum install mariadb-server.x86_64 -y
-systemctl enable mariadb
-systemctl start mariadb
-# systemctl status mariadb
+# Installing MariaDB
+sudo yum install mariadb105-server.x86_64 -y
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+#systemctl status mariadb
 
 git clone https://gitlab.com/rns-app/student-app.git
