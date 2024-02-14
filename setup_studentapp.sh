72f87e314d451b604d7376bacb0ad3643d230d5f
Jagadeswar Reddy Puthi <148185310+CloudJagadesh@users.noreply.github.com>
2024-02-14 12:14:17 +0530

    Update setup_studentapp.sh

--- setup_studentapp.sh
+++ setup_studentapp.sh
@@ -4,7 +4,7 @@ cd /opt/student-app
 
 git pull origin master
 
-sudo mysql -uroot < /opt/student-app/dbscript/studentapp.sql
+mysql -uroot < /opt/student-app/dbscript/studentapp.sql
 
 # Manager's App Context XML
 
@@ -31,12 +31,11 @@ sudo systemctl start tomcat
 
 cd /opt/student-app/
 
-
-echo 1 | sudo alternatives --config java
-
-sudo su - devops -c "cd /opt/student-app/ && mvn clean package"
-
 echo 2 | sudo alternatives --config java
+
+mvn clean package
+
+echo '1' | sudo alternatives --config java
  
 cp /opt/student-app/target/*.war /opt/tcserver/webapps/student.war
 
